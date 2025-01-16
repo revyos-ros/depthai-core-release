657cf5be56875f01abd2c38b6885d8ebac6a777e
Zheng Junjie <873216071@qq.com>
2025-01-17 00:25:06 +0800

    Rebase from 'debian/humble/depthai'

--- shared/depthai-shared/include/depthai-shared/datatype/RawCameraControl.hpp
+++ shared/depthai-shared/include/depthai-shared/datatype/RawCameraControl.hpp
@@ -437,6 +437,7 @@ struct RawCameraControl : public RawBuffer {
     uint16_t wbColorTemp;    // 1000 .. 12000
     uint8_t lowPowerNumFramesBurst;
     uint8_t lowPowerNumFramesDiscard;
+    std::vector<std::pair<std::string, std::string>> miscControls;
 
     void setCommand(Command cmd, bool value = true) {
         uint64_t mask = 1ull << (uint8_t)cmd;
@@ -493,7 +494,8 @@ struct RawCameraControl : public RawBuffer {
                       chromaDenoise,
                       wbColorTemp,
                       lowPowerNumFramesBurst,
-                      lowPowerNumFramesDiscard);
+                      lowPowerNumFramesDiscard,
+                      miscControls);
 };
 
 }  // namespace dai
